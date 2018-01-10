461fc977c7a71d42875397b1ea04de8d27f4f2ec
Sandeep Murthy <smurthy@protonmail.ch>
2018-01-10 11:26:58 +0000

    Update `oasis_utils` submodule + add master script stub

---
+++ run_model.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Master script for end-to-end model run
+"""
+
+import argparse
+import logging
+import os
+import sys
+
+if __name__ == '__main__':
+	pass
