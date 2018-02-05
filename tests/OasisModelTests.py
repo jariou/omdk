233cd7753e2894cfa1bc9c0d65659e4b5407cbe7
Sandeep Murthy <smurthy@protonmail.ch>
2018-02-05 18:39:40 +0000

    Complete unit tests for `models.OasisModel` and `models.OasisModelFactory`

--- tests/OasisModelTests.py
+++ tests/OasisModelTests.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 """
@@ -6,3 +7,100 @@ Python unit tests for ::
     omdk.models.OasisModel
     omdk.models.OasisModelFactory
 """
+
+import csv
+import io
+import os
+import sys
+import unittest
+
+sys.path.insert(0, os.path.abspath(os.pardir))
+
+from models import (
+    OasisModel,
+    OasisModelFactory
+)
+
+
+class OasisModelTests(unittest.TestCase):
+
+    def setUp(self):
+
+        self.tests_datadir = os.path.join(os.getcwd(), 'data', 'PiWind')
+
+        with io.open(os.path.join(self.tests_datadir, 'ModelVersion.csv'), 'r', encoding='utf-8') as f:
+            self.test_model_supplier_id, self.test_model_id, self.test_model_version_id = map(lambda s: s.strip(), map(tuple, csv.reader(f))[0])
+
+    
+    def test_model_object_creation_with_no_resources(self):
+
+        test_model = OasisModel(
+            self.test_model_supplier_id,
+            self.test_model_id,
+            self.test_model_version_id
+        )
+
+        self.assertIsInstance(test_model, OasisModel)
+
+        self.assertEquals(test_model.supplier_id, self.test_model_supplier_id)
+        self.assertEquals(test_model.model_id, self.test_model_id)
+        self.assertEquals(test_model.model_version_id, self.test_model_version_id)
+
+
+    def test_model_object_creation_with_resources(self):
+
+        model_version_file_path = os.path.join(self.tests_datadir, 'ModelVersion.csv')
+        resources = {'model_version_file_path': model_version_file_path}
+        test_model = OasisModel(
+            self.test_model_supplier_id,
+            self.test_model_id,
+            self.test_model_version_id,
+            resources=resources
+        )
+
+        self.assertIsInstance(test_model, OasisModel)
+
+        self.assertEquals(test_model.supplier_id, self.test_model_supplier_id)
+        self.assertEquals(test_model.model_id, self.test_model_id)
+        self.assertEquals(test_model.model_version_id, self.test_model_version_id)
+
+        self.assertEquals(test_model.resources, resources)
+
+
+    def test_model_object_creation_with_no_resources_via_factory(self):
+
+        test_model = OasisModelFactory.create(
+            model_supplier_id=self.test_model_supplier_id,
+            model_id=self.test_model_id,
+            model_version_id=self.test_model_version_id
+        )
+
+        self.assertIsInstance(test_model, OasisModel)
+
+        self.assertEquals(test_model.supplier_id, self.test_model_supplier_id)
+        self.assertEquals(test_model.model_id, self.test_model_id)
+        self.assertEquals(test_model.model_version_id, self.test_model_version_id)
+
+
+    def test_model_object_creation_with_resources_via_factory(self):
+
+        model_version_file_path = os.path.join(self.tests_datadir, 'ModelVersion.csv')
+        resources = {'model_version_file_path': model_version_file_path}
+        test_model = OasisModelFactory.create(
+            model_supplier_id=self.test_model_supplier_id,
+            model_id=self.test_model_id,
+            model_version_id=self.test_model_version_id,
+            resources=resources
+        )
+
+        self.assertIsInstance(test_model, OasisModel)
+
+        self.assertEquals(test_model.supplier_id, self.test_model_supplier_id)
+        self.assertEquals(test_model.model_id, self.test_model_id)
+        self.assertEquals(test_model.model_version_id, self.test_model_version_id)
+
+        self.assertEquals(test_model.resources, resources)
+
+
+if __name__ == '__main__':
+    unittest.main()
